2f26cfbd82ea70845259368df73ab597e1fce6fb
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-25 14:20:11 +0300

    seed yazıldı

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,19 @@
 // ESNEK
+/**
+ *@param {import ("knex").Knex} knex
+ *@returns {Promise<void>}
+ */
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert([
+    {
+      vin: "02A215",
+      make: "Toyota",
+      model: "Corolla",
+      mileage: 12,
+      title: "temiz",
+      transmission: "manuel",
+    },
+  ]);
+};
