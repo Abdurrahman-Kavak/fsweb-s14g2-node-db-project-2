68ad7b50265e5c5a183aeacacbe47c6d77a76fb4
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-25 14:28:44 +0300

    cars model yazıldı

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,16 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // HOKUS POKUS
-}
+  return db("cars");
+};
 
 const getById = () => {
   // HOKUS POKUS
-}
+  return db("cars").where("id", id).first();
+};
 
 const create = () => {
   // HOKUS POKUS
-}
+  return db("cars").insert(car);
+};
