5488e27b66702cae04bdc32d4e7be03784e4bfc3
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-25 14:16:20 +0300

    cars tablosu oluşturuldu

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  knex.schema.createTable("cars", (table) => {
+    table.increments("id").primary();
+    table.string("vin").notNullable().unique();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.string("title");
+    table.string("transmission");
+  });
 };
 
 exports.down = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  knex.schema.dropTableIfExists("cars");
 };
