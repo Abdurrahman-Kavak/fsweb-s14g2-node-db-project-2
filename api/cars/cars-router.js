ad5658777734e1e54db51d2ff716ce098eda83b8
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-25 14:54:43 +0300

    router yazıldı.

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,41 @@
 // HOKUS POKUS
+const router = require("express").Router();
+
+const { getAll, getById, create } = require("./cars-model");
+
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+
+router.get("/", (req, res, next) => {
+  getAll()
+    .then((cars) => {
+      res.status(200).json(cars || []);
+    })
+    .catch(next);
+});
+router.get("/:id", checkCarId, (req, res, next) => {
+  getById(req.params.id)
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch(next);
+});
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  (req, res, next) => {
+    create(req.body)
+      .then(([id]) => {
+        res.status(201).json({ id });
+      })
+      .catch(next);
+  },
+);
+
+module.exports = router;
